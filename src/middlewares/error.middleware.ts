0243a5c6fe46f361b44b24a3fc7b4b3620a340db
ezann <rayzanmuhammadr774@gmail.com>
2026-02-07 23:13:28 +0700

    refactor: add global error handler and custom app error

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/appError";
+
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  res.status(500).json({
+    message: "Internal server error",
+  });
+};
